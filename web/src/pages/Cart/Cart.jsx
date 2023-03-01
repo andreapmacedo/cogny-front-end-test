56d8ad224812d69ab4d231c1431421cb8c5d4e0c
andreapmacedo <aa.andremacedo@gmail.com>
2023-02-28 22:57:30 -0300

    Feat: Adiciona página 'Cart'.

---
+++ web/src/pages/Cart/Cart.jsx
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+import { projectFirestore } from '../../firebase/config';
+
+const Cart = () => {
+
+
+  return (
+    <div
+      className="cart"
+    >
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+
+
+export default Cart;
