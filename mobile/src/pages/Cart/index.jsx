91e34c114b13ac0e9a3a7bc3861918fe6678ae2a
andreapmacedo <aa.andremacedo@gmail.com>
2023-03-02 13:08:35 -0300

    [mobile] Feat:  adiciona rotas.

---
+++ mobile/src/pages/Cart/index.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+import { View, Text } from 'react-native'
+// import { VStack } from 'native-base'
+
+export default function Cart() {
+  return (
+    <View>
+      <Text>Cart</Text>
+    </View>
+  )
+}
